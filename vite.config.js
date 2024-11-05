0674f83225109c3296a8e8592d818588b8952f58
rit-123 <diamondfox2264@gmail.com>
2024-11-04 19:42:55 -0600

    sdfsdf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/English-Creative-Project/',  // Replace with your actual repository name
   plugins: [react()],
 })
